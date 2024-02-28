d6520c1ff0656a30c8eb34b8128004eb28df547a
ordila <ordila99@gmail.com>
2024-02-28 15:05:25 +0200

    add themes

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { Link } from "react-router-dom";
 
 import { Suspense, lazy } from "react";
 
-import "./index.scss";
+import "./styles/index.scss";
+
+import { useTheme } from "./theme/useTheme";
 
 const MainPageLazy = lazy(() => import("./pages/MainPage/MainPage"));
 const AboutPageLazy = lazy(() => import("./pages/AboutPage/AboutPage"));
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}> Toggle</button>
       <Link to="/">Home</Link>
       <Link to="/about"> About</Link>
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,13 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 import { render } from "react-dom";
+import ThemeProvider from "./theme/ThemeProvider";
 
 render(
   <BrowserRouter>
-    <div>
+    <ThemeProvider>
       <App />
-    </div>
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
